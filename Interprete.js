2c04682ab902be3adc4b19b3209a1fb83a8924ef
Hermes-Francisco <55466449+Hermes-Francisco@users.noreply.github.com>
2020-03-14 10:55:55 -0300

    Add files via upload

---
+++ Interprete.js
@@ -0,0 +1,108 @@
+class Interprete{
+
+    nExtenso(indice){
+       indice = indice.split(",");
+
+       const extenso = [
+            ["zero","um","dois","três","quatro","cinco","seis","sete","oito","nove","dez",
+             "onze","doze","treze","quatorze","quinze","dezesseis","dezessete","dezoito","dezenove"],
+            ["","","vinte","trinta","quarenta","cinquenta","sessenta","setenta","oitenta","noventa"],
+            ["cem","cento","duzentos","trezentos","quatrocentos","quinhentos","seiscentos","setecentos",
+             "oitocentos","novecentos"]];
+
+        var word = extenso[indice[0]][indice[1]];
+        if(word == "cem")indice[1] = 1;
+        return [word, indice[0], indice[1]];
+    }
+    toAlgarismo(numero){
+        
+        var numero = numero.toLowerCase();
+        var palavra = numero.split(" ");
+
+        var sinal = 1;
+        var soma = 0;
+        var virgula = "-";
+        var erro = false;
+        
+        for(var i = 0; i < palavra.length; i++){
+                   
+            if(palavra[i] == "menos"){sinal = -1; i++}
+            if(palavra[i] == "e") {i++};
+            if(palavra[i] == "vírgula" || palavra[i] == "ponto"){
+                virgula = soma.toString() + "."; 
+                soma = 0; 
+                i++;
+                if(palavra [i] == "zero"){soma = "0"; i++}
+            }
+                        
+            for(var j = 0; j < this.index(palavra[i]).length; j++){
+
+                var indice = this.index(palavra[i])[j];
+                
+                if(palavra[i] == this.nExtenso(indice)[0]){
+                    
+                    indice = this.nExtenso(indice);
+                    soma += (indice[2] * Math.pow(10,indice[1]));
+                    console.log(soma);
+                    break;
+                
+                }
+                
+                if(j == this.index(palavra[i]).length - 1)erro = true;
+            }
+            if(erro == true){alert( 'erro "' + palavra[i] + '".');return "erro"}         
+        }
+
+        return ((virgula != "-")?virgula + soma.toString(): soma)* sinal;
+    }
+    index(palavra){
+        
+        var inicio = palavra.substring(0,1);
+
+        if(inicio == "z") return ["0,0"];
+        if(inicio == "u") return ["0,1"];
+        if(inicio == "d") return ["0,2","0,10","0,12","0,16","0,17","0,18","0,19","2,2"]
+        if(inicio == "o") return ["0,8","0,11","1,8","2,8"];
+        if(inicio == "c") return ["0,5","1,5","2,0","2,1"];
+        if(inicio == "v") return ["1,2"];
+        if(inicio == "t") return ["0,3","0,13","1,3","2,3"];
+        if(inicio == "q") return ["0,4","0,14","0,15","1,4","2,4","2,5"];
+        if(inicio == "s") return ["0,6","0,7","1,6","1,7","2,6","2,7"];
+        if(inicio == "n") return ["0,9","1,9","2,9"];
+
+        return ["0,0"];
+    }
+
+    toExtenso(numero){
+       
+        if(isNaN(parseInt(numero))) return "erro";
+        if(numero > 999.99 || numero < -999.99) return numero + " (fora do alcance do intérprete)"
+
+        var v = ""
+        if(parseInt(numero) != parseFloat(numero)){
+            numero = parseFloat(numero);
+            v = parseFloat(numero.toFixed(2));
+            console.log("calc "+ v)
+            v = v.toString().split(".");
+        }
+        return this.toExtenso1(numero) + ((v != "")?" vírgula " + ((v[1].substring(0,1) == "0")? "zero " : "") 
+        + this.toExtenso1(v[1]):"")
+    }
+    toExtenso1(numero){
+        
+        var frase = "";
+        if (numero < 0){frase = "menos "; numero *= -1;} 
+
+        if(numero == 100) return frase + "cem";
+
+        var centenas = parseInt(numero / 100);
+        var dezenas = parseInt((numero % 100) / 10);
+        var unidades = parseInt((numero % 100) % 10);
+      
+        if(centenas > 0)frase += this.nExtenso("2,"+centenas)[0] + ((dezenas > 0 || unidades > 0)? " e " : "");
+        if(dezenas == 1) return frase + this.nExtenso("0,1"+unidades)[0];
+        if(dezenas > 1) frase += this.nExtenso("1,"+dezenas)[0] + ((unidades > 0)? " e ": "");
+        if(dezenas == 0 && centenas == 0) return frase + this.nExtenso("0,"+unidades)[0];
+        return frase + ((unidades > 0)? this.nExtenso("0,"+unidades)[0]: "");
+    }    
+}
---
+++ calculadora.js
@@ -0,0 +1,63 @@
+function prevent(e){
+    e.preventDefault();
+}
+
+document.getElementById("formulario").addEventListener('submit', prevent);
+
+var num = document.getElementById("texto");
+
+i = new Interprete();
+    
+function interpretar(numero){
+    return (!isNaN(parseInt(numero)))? iniMaiuscula(i.toExtenso(numero)) : i.toAlgarismo(numero);
+}
+
+function apagar(){num.value = ""}
+function soma(){num.value += " mais "; num.focus()}
+function subitracao(){num.value += " menos "; num.focus()}
+function multiplicacao(){num.value += " vezes "; num.focus()}
+function divisao(){num.value += " dividido por "; num.focus()}
+
+function iniMaiuscula(palavra){
+    return palavra.substring(0,1).toUpperCase() + palavra.substring(1);
+}
+
+var n1;
+var operation;
+var n2;
+
+function separation(frase){
+    
+    frase = frase.toLowerCase();
+    var palavra = frase.split(" ");
+    
+    for(var i = 0; i < palavra.length; i++){
+        if(i == 0)i++;
+        var p = palavra[i];
+        if(p == "mais"||p == "menos"||p == "vezes"||p == "dividido"){
+            operation = p + ((p == "dividido")? " por":"");
+            break;
+        }
+        if(i == palavra.length - 1) alert("Você não escreveu a operação, ou a escreveu errado.")
+    }
+    frase = frase.split(" " + operation + " ");
+    n1 = frase[0];
+    n2 = frase[1];
+        
+}
+function calculo(){
+    separation(num.value);
+    if (n1.split(",").length > 1) n1 = n1.split(",")[0] + "." + n1.split(",")[1];
+    if (n2.split(",").length > 1) n2 = n2.split(",")[0] + "." + n2.split(",")[1];
+    if (isNaN(n1)) n1 = interpretar(n1);
+    if (isNaN(n2)) n2 = interpretar(n2);
+
+    if(operation == "mais") num.value = interpretar(n1 + n2);
+    if(operation == "menos") num.value = interpretar(n1 - n2);
+    if(operation == "vezes") num.value = interpretar(n1 * n2);
+    if(operation == "dividido por") num.value = interpretar(n1 / n2);
+}
+function regras(){
+    alert("Você pode escrever '10,5 vezes dois'; 'dez vírgula cinco vezes 2'; ou 'dez vírgula cinco vezes dois', por exemplo. \nO maior número aceito é 999,99 e o menor é -999,99. \n"+
+    "A precisão máxima é de duas casas decimais - numeros com mais casas decimais são arredondados.\n\nProgramado por: Hermes Francisco. Data: 14/03/2020")
+}
